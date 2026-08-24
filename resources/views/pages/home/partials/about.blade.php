80b402f75d77b53703ff6867ebcd62305a3b7635
PhamMinh <webmaster2.hbg@gmail.com>
2026-08-24 17:05:10 +0700

    Add new images and update about section: include 'about-vung-tau-check.png', 'about-vung-tau-desktop.png', and 'about-vung-tau-stacked.png' images, enhance the about section layout with a new header and language switcher, and improve CSS for better presentation.

--- resources/views/pages/home/partials/about.blade.php
+++ resources/views/pages/home/partials/about.blade.php
@@ -1,7 +1,18 @@
   <!-- Home About Section Begin -->
   <section class="home-about-section spad" x-data="{ lang: 'vi' }">
     <div class="container">
-      <div class="row">
+      <div class="ha-header">
+        <div class="about-section-title">
+          <h2 class="main">Về Vũng Tàu</h2>
+          <div class="sub">About Vung Tau</div>
+        </div>
+        <div class="switch-language">
+          <button @click="lang = 'vi'" :class="{ 'active': lang === 'vi' }" class="button-vietnam" type="button">Tiếng Việt</button>
+          <button @click="lang = 'en'" :class="{ 'active': lang === 'en' }" class="button-english" type="button">English</button>
+        </div>
+      </div>
+
+      <div class="row ha-content-row">
         <div class="col-lg-6">
           <div class="ha-pic">
             <iframe src="https://www.youtube.com/embed/bB_mm7cI-6k?autoplay=1&mute=1&loop=1&playlist=bB_mm7cI-6k&vq=hd1080&rel=0" title="YouTube video player" frameborder="0"
@@ -11,14 +22,6 @@
         </div>
         <div class="col-lg-6">
           <div class="ha-text">
-            <div class="about-section-title">
-              <h2 class="main">Về Vũng Tàu</h2>
-              <div class="sub">About Vung Tau</div>
-            </div>
-            <div class="switch-language">
-              <button @click="lang = 'vi'" :class="{ 'active': lang === 'vi' }" class="button-vietnam" type="button">Tiếng Việt</button>
-              <button @click="lang = 'en'" :class="{ 'active': lang === 'en' }" class="button-english" type="button">English</button>
-            </div>
             <div class="ha-about-body" x-show="lang === 'vi'" x-cloak>
               <p>Vũng Tàu là một thành phố biển xinh đẹp ở miền Nam Việt Nam, nằm cách Thành phố Hồ Chí Minh khoảng 90 km về phía Đông Nam. Nổi tiếng với những bãi biển đẹp, hải sản tươi ngon và không khí thư giãn, Vũng Tàu là điểm đến được yêu thích của cả du khách trong nước và quốc tế.</p>
               <p>Thành phố là sự kết hợp hài hòa giữa vẻ đẹp thiên nhiên, các điểm tham quan văn hóa và hệ thống dịch vụ du lịch hiện đại. Du khách có thể tận hưởng khung cảnh biển tuyệt đẹp, khám phá các địa danh nổi tiếng như Tượng Chúa Kitô Vua và Hải đăng Vũng Tàu, hoặc thư giãn tại những bãi biển của thành phố.</p>
